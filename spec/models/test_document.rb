6f9ba67080995f2ebd31f4b83a9bd7cb9ba6b8b5
Brandon Turner <bturner@bltweb.net>
2011-08-26 18:33:07 -0500

    Cleanup attribute setting in tests

--- spec/models/test_document.rb
+++ spec/models/test_document.rb
@@ -21,10 +21,11 @@ module Mongoid::Acts::NestedSet
 
       def test_set_attributes(attrs)
         attrs.each do |key, val|
+          key = key.to_s
           if Mongoid.allow_dynamic_fields ||
-              fields.keys.any? { |k| k.to_s == key.to_s } ||
-              associations.any? { |a| a[0].to_s == key.to_s || a[1].foreign_key.to_s == key.to_s }
-            @attributes[key] = val
+              fields.keys.any? { |k| k.to_s == key } ||
+              associations.any? { |a| a[0].to_s == key || a[1].foreign_key.to_s == key }
+            @attributes[key] = self.typed_value_for(key, val)
           end
         end
         self
