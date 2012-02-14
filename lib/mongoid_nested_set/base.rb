10148b60322b06402d185916506d6e07cc65d61c
Alexey Savartsov <asavartsov@gmail.com>
2012-02-14 11:09:17 +0400

    Formatting fix

--- lib/mongoid_nested_set/base.rb
+++ lib/mongoid_nested_set/base.rb
@@ -48,8 +48,8 @@ module Mongoid::Acts::NestedSet
         field outline_number_field_name, :type => String if outline_number_field_name
         field :depth, :type => Integer
 
-        has_many :children, :class_name => self.name, :foreign_key => parent_field_name, :inverse_of => :parent, :order => criteria.asc(left_field_name)
-        belongs_to   :parent,   :class_name => self.name, :foreign_key => parent_field_name
+        has_many   :children, :class_name => self.name, :foreign_key => parent_field_name, :inverse_of => :parent, :order => criteria.asc(left_field_name)
+        belongs_to :parent,   :class_name => self.name, :foreign_key => parent_field_name
 
         attr_accessor :skip_before_destroy
 
