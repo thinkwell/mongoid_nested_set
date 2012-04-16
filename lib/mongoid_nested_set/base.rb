a837cdca18933f76c96919694ef632f1df41ab75
Alexey Savartsov <asavartsov@gmail.com>
2012-04-16 19:35:44 +0400

    `Origin` format for has_many order option

--- lib/mongoid_nested_set/base.rb
+++ lib/mongoid_nested_set/base.rb
@@ -48,7 +48,7 @@ module Mongoid::Acts::NestedSet
         field outline_number_field_name, :type => String if outline_number_field_name
         field :depth, :type => Integer
 
-        has_many   :children, :class_name => self.name, :foreign_key => parent_field_name, :inverse_of => :parent, :order => criteria.asc(left_field_name)
+        has_many   :children, :class_name => self.name, :foreign_key => parent_field_name, :inverse_of => :parent, :order => left_field_name.to_sym.asc
         belongs_to :parent,   :class_name => self.name, :foreign_key => parent_field_name
 
         attr_accessor :skip_before_destroy
